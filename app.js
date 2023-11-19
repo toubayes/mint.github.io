7ee60eaa0a8820aa2ccb99d8f299e60b7bcfda82
youcelaala <63110250+toubayes@users.noreply.github.com>
2023-11-19 07:52:03 -0800

    projst-2-

---
+++ app.js
@@ -0,0 +1,9 @@
+var drodoen =document.querySelector(".dropdown-menu");
+var droplist=document.querySelector(".dropdown-list");
+
+ 
+
+function myFunction() {
+    droplist.classList.toggle("show");
+  }
+  
